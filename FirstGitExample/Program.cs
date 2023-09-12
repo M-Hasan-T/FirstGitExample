9ec0cfa95a958d77ca0be3d80dc7158c84bc9bce
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-09-12 14:11:32 +0200

    Add project files.

---
+++ FirstGitExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstGitExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
